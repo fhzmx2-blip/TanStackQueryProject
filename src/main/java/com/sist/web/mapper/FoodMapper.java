c25b9fceb7822929b2240a650778c00cc58857de
fhzmx2-blip <fhzmx2@gmail.com>
2026-09-18 16:30:11 +0900

    food data

---
+++ src/main/java/com/sist/web/mapper/FoodMapper.java
@@ -0,0 +1,37 @@
+package com.sist.web.mapper;
+
+import java.util.*;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.sist.web.vo.FoodVO;
+
+@Mapper
+@Repository
+public interface FoodMapper {
+   /*
+    *   <select id="foodListData" 
+		     resultType="com.sist.web.vo.FoodVO" parameterType="hashmap">
+		     SELECT no,poster,name,score,theme,type
+		     FROM food
+		     <if test="search!=null and search!=''">
+		       WHERE address LIKE CONCAT('%',#{search},'%')
+		     </if>
+		     ORDER BY no ASC
+		     OFFSET #{start} ROWS FETCH NEXT 12 ROWS ONLY
+		  </select>
+      */
+	  public List<FoodVO> foodListData(Map map);
+	  /*
+	  <select id="foodListTotalPage" 
+	     resultType="int" parameterType="string">
+	     SELECT COUNT(*)
+	     FROM food
+	     <if test="search!=null and search!=''">
+	       WHERE address LIKE CONCAT('%',#{search},'%')
+	     </if>
+	  </select>
+    */
+	  public int foodListTotalPage(String search);
+}

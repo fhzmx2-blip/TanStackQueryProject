4baac049bd5eb16815664cbc1b5368dce771b1cc
fhzmx2-blip <fhzmx2@gmail.com>
2026-09-18 14:07:39 +0900

    ready for cicd

--- src/main/java/com/sist/web/controller/RouterController.java
+++ src/main/java/com/sist/web/controller/RouterController.java
@@ -1,12 +1,14 @@
 package com.sist.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RouterController {
    @GetMapping("/main")
-   public String main_page() {
+   public String main_page(Model model) {
+	   model.addAttribute(model);
 	   return "main/main";
    }
 }
